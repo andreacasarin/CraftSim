934b5420a31d9a44fdbdda3f38cb74dd03b423ad
Andrea Casarin <git@andreacasarin.com>
2026-03-13 19:28:29 +0100

    fix: correct conditional structure for zero concentration queuing in favorites

--- Modules/CraftQueue/CraftQueue.lua
+++ Modules/CraftQueue/CraftQueue.lua
@@ -540,8 +540,11 @@ function CraftSim.CRAFTQ:QueueFavorites()
                         break -- only queue first recipe in this mode
                     end
                 else
-                if CraftSim.DB.OPTIONS:Get("CRAFTQUEUE_RESTOCK_FAVORITES_ZERO_CONCENTRATION_QUEUING") and recipeData.averageProfitCached > 0 then
-                    CraftSim.CRAFTQ:AddRecipe { recipeData = recipeData, amount = 1 + offsetAmount }
+                    local offsetAmount = tonumber(CraftSim.DB.OPTIONS:Get(
+                        "CRAFTQUEUE_QUEUE_FAVORITES_OFFSET_QUEUE_AMOUNT"))
+                    if CraftSim.DB.OPTIONS:Get("CRAFTQUEUE_RESTOCK_FAVORITES_ZERO_CONCENTRATION_QUEUING") and recipeData.averageProfitCached > 0 then
+                        CraftSim.CRAFTQ:AddRecipe { recipeData = recipeData, amount = 1 + offsetAmount }
+                    end
                 end
             end
 
